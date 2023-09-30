75eb68a63192658ef8bc1ea4fa21f7c4f2516c80
Allister Maguire <allister@maguir.es>
2023-10-01 10:52:17 +1300

    Use constant_program_id for Smart Program switch unique_id

--- custom_components/bhyve/util.py
+++ custom_components/bhyve/util.py
@@ -1,3 +1,4 @@
+import hashlib
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.util import dt
 
@@ -23,3 +24,12 @@ def filter_configured_devices(entry: ConfigEntry, all_devices):
             device["name"] = "Unknown Device"
 
     return filtered_devices
+
+def constant_program_id(device_id, program_id, is_smart_program: bool = False):
+    """For devices with multiple zones, Smart program id changes depending on the zone/s that are included.
+    Generate a constant id so that it is updated on change."""
+    if is_smart_program:
+        program_id = hashlib.md5(
+            "{}:smart_program".format(device_id).encode("utf-8")
+        ).hexdigest()
+    return program_id
